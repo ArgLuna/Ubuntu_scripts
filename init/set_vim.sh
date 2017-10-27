418ae4376f192fc68494840b7f374c3fe7820b04
ArgLuna <ian955246@gmail.com>
2017-10-27 17:38:30 +0800

    Edit msg and fix malfunction
    Make echo messages more clear.
    In prev ver, there're some problem on path about installing YCM.
    YCM should be installednow.

--- init/set_vim.sh
+++ init/set_vim.sh
@@ -1,19 +1,24 @@
-echo Start to setup vim
-echo copy .vimrc
-cp ./vimrc ~/.vimrc
-echo Enter ~/.vim/bundle
+echo ">>Start to setup vim"
+echo ">>Copy .vimrc"
+cp ./vimrc ~/.vimrc -v
+echo ">>Enter ~/.vim/bundle"
 cd ~/.vim/bundle || exit
 
-echo Install required packages for YCM
+echo ">>Install required packages for YCM"
 apt-get install -y build-essential cmake python-dev python3-dev clang
 
-echo Install Vundle and plugins
+echo ">>Install Vundle and plugins"
+echo ">>Plugin list:"
+echo ">>** VundleVim/Vundle.vim"
+echo ">>** Valloric/YouCompleteMe"
+echo ">>** ntpeters/vim-better-whitespace"
 git clone https://github.com/VundleVim/Vundle.vim.git
 vim +PluginInstall +qall
 
-echo Compile YCM and setup config
-../YouCompleteMe/install.py --clang-completer
+echo ">>Compile YCM and setup config"
+cd ~/.vim/bundle/YouCompleteMe
+./install.py --clang-completer
 cp third_party/ycmd/examples/.ycm_extra_conf.py plugin/
 
-echo Finished! Return to home directory
+echo ">>Finished! Return to home directory"
 cd
