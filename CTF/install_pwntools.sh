28f791e4ce230447fe4a3a2f05b2e37f93359c29
ArgLuna <ian955246@gmail.com>
2017-11-02 16:51:36 +0800

    Seperate by function

--- CTF/install_pwntools.sh
+++ CTF/install_pwntools.sh
@@ -1,13 +1,7 @@
+#!/bin/bash
+
 apt-get update
 apt-get install -y python-pip || exit
 pip install pwntools
 pip install --upgrade capstone
 
-apt-get install -y ipython python-tk
-
-apt-get install -y nmap
-
-git clone https://github.com/longld/peda.git ~/peda
-echo "source ~/peda/peda.py" >> ~/.gdbinit
-
-apt-get install -y gcc-multilib
---
+++ CTF/tmp.sh
@@ -0,0 +1,6 @@
+apt-get install -y ipython python-tk
+
+apt-get install -y nmap
+
+apt-get install -y gcc-multilib
+
