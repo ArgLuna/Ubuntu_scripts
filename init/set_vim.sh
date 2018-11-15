df885662a13f21a42f94219dc6c4525c624ce20f
ArgLuna <ian955246@gmail.com>
2018-11-15 16:38:07 +0800

    Remove vim plugin: YCM

--- init/set_vim.sh
+++ init/set_vim.sh
@@ -1,26 +1,21 @@
 echo ">>Start to setup vim"
 echo ">>Updating..."
 apt-get update || exit
+
+echo ">>Check ~/.vim/bundle"
+ls ~/.vim || (echo "directory not found" ; mkdir -v ~/.vim)
+ls ~/.vim/bundle || (echo "directory not found" ; mkdir -v ~/.vim/bundle)
+
 echo ">>Copy .vimrc"
 cp ./vimrc ~/.vimrc -v
-echo ">>Enter ~/.vim/bundle"
-cd ~/.vim/bundle || exit
-
-echo ">>Install required packages for YCM"
-apt-get install -y build-essential cmake python-dev python3-dev clang
 
 echo ">>Install Vundle and plugins"
 echo ">>Plugin list:"
 echo ">>** VundleVim/Vundle.vim"
-echo ">>** Valloric/YouCompleteMe"
 echo ">>** ntpeters/vim-better-whitespace"
-git clone https://github.com/VundleVim/Vundle.vim.git
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
-echo ">>Compile YCM and setup config"
-cd ~/.vim/bundle/YouCompleteMe
-YCM_CORES=1 ./install.py --clang-completer
-cp third_party/ycmd/examples/.ycm_extra_conf.py plugin/
-
-echo ">>Finished! Return to home directory"
-cd
+echo ">>Finished!"
+echo "Use the vim command to check vundle installation:"
+echo ":h vundle"
