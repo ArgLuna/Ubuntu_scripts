abb5b497b6d89eb144d084e8dcecbf0baf91e89d
ArgLuna <ian955246@gmail.com>
2018-11-29 09:09:33 -0800

    Combine peda and Pwngdb

--- CTF/Pwngdb.sh
+++ CTF/Pwngdb.sh
@@ -1,3 +1,5 @@
-cd ~/
-git clone https://github.com/scwuaptx/Pwngdb
+git clone https://github.com/longld/peda.git ~/peda
+echo "source ~/peda/peda.py" >> ~/.gdbinit
+
+git clone https://github.com/scwuaptx/Pwngdb ~/Pwngdb
 cp ~/Pwngdb/.gdbinit ~/
