c2871b32c937ae38d6eb7e7156312872d3ebf2dc
ArgLuna <ian955246@gmail.com>
2019-02-27 21:38:22 +0800

    Install and setup zsh

---
+++ init/zsh/set_zsh.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo ">>Installing zsh..."
+apt-get install -y zsh || exit
+
+echo ">>Shell test"
+which zsh || exit
+
+echo ">>Installing oh-my-zsh..."
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+echo ">>Installing powerline fonts..."
+apt-get install -y fonts-powerline
+
+echo ">>Copy .zshrc"
+cp ~/Ubuntu_scripts/init/zsh/zshrc ~/.zshrc -v
+
+echo ">>Set default shell..."
+chsh -s `which zsh`
+
+echo ">>Finish!"
