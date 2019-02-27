b42d7d6c16b3094dd4407a404792dab29c347047
ArgLuna <ian955246@gmail.com>
2019-02-27 21:36:59 +0800

    Install powerline fonts and change conf path

--- init/tmux/set_tmux.sh
+++ init/tmux/set_tmux.sh
@@ -1,7 +1,11 @@
+#!/bin/bash
 echo ">>Installing..."
 apt-get install -y tmux || exit
 
+echo ">>Installing powerline fonts..."
+apt-get install -y fonts-powerline
+
 echo ">>Copy .tmux.conf"
-cp ~/Ubuntu_scripts/init/tmux.conf ~/.tmux.conf -v
+cp ~/Ubuntu_scripts/init/tmux/tmux.conf ~/.tmux.conf -v
 
 echo ">>Finish!"
