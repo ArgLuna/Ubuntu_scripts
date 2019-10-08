bf8a092aad1b59155c5495dbcbebbdc5ebfb0c94
ArgLuna <ian955246@gmail.com>
2019-10-08 02:21:06 +0000

    Add home/end keybinding. Update set_zsh.sh

--- init/zsh/set_zsh.sh
+++ init/zsh/set_zsh.sh
@@ -9,7 +9,11 @@ echo ">>Installing oh-my-zsh..."
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
 echo ">>Copy .zshrc"
-cp ~/Ubuntu_scripts/init/zsh/zshrc ~/.zshrc -v
+cp ~/Ubuntu_scripts/init/zsh/zshrc ~/zshrc -v
+cd ~/
+echo "  export ZSH=\"`pwd`/.oh-my-zsh\"" > ~/.zshrc
+cat ~/zshrc >> ~/.zshrc
+rm ~/zshrc
 
 echo ">>Set default shell..."
 chsh -s `which zsh`
